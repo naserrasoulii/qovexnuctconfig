5c6154c27a2d8d5c19bd9f6fafe0a0284c09af2e
Naser Rasouli <nrasoli3@gmail.com>
2023-03-22 19:46:12 +0430

    'startProjects'

---
+++ app.vue
@@ -0,0 +1,11 @@
+<template>
+ <NuxtPage></NuxtPage>
+</template>
+<script setup>
+// useHead({
+//   link:[{href:'~/css/app.css',rel:'stylesheet'}],
+//   script:[{
+//     src:'~/js/app.js'
+//   }]
+// })
+</script>
